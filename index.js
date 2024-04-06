3bead5258937ca6aecfa5ae4350e174d605e3b51
Jason Fill <jason@smarterservices.com>
2024-04-06 07:58:48 -0400

    Updated the configuraiton.

--- index.js
+++ index.js
@@ -3,23 +3,13 @@ const sendToApiTraffic = require('./lib/sendToApiTraffic');
 const handleResponseBody = require('./lib/handleResponseBody');
 const outboundInterception = require('./lib/outboundInterception');
 const setup = require('./lib/setup');
+const config = require('./lib/config');
 
-if(!process.env.API_TRAFFIC_INGEST_HOST){
-    process.env.API_TRAFFIC_INGEST_HOST = "ingest.apitraffic.io";
-}
-/*
-
-if(!process.env.API_TRAFFIC_TOKEN){
-    throw(`An APITraffic API Token must be set in the env variables as API_TRAFFIC_TOKEN.`);
-}
-if(!process.env.API_TRAFFIC_BUCKET_SID){
-    throw(`An APITraffic Bucket Sid must be set in the env variables as API_TRAFFIC_BUCKET_SID.`);
-}
-*/
 module.exports = {
     getDuration,
     sendToApiTraffic,
     handleResponseBody,
     outboundInterception,
-    setup
+    setup,
+    config
 }
